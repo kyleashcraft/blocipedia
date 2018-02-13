5a10fa19f28b1f79360f037627e9b90057c35ac3
kyleashcraft <ashcraft.k@husky.neu.edu>
2018-02-12 19:11:46 -0800

    user added with devise, wiki model created

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,20 @@
 Rails.application.routes.draw do
+  get 'wiki/index'
+
+  get 'wiki/show'
+
+  get 'wiki/new'
+
+  get 'wiki/edit'
+
+  get 'wiki/create'
+
+  get 'wiki/read'
+
+  get 'wiki/update'
+
+  get 'wiki/delete'
+
   root 'welcome#index'
 
   devise_for :users
